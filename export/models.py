6538342f1d72b82fd8a7fa9b25e84abb67a9c055
kormosi <kormi.patres@gmail.com>
2020-03-11 11:26:44 +0100

    commit message: create and register Guest model, add __str__ dunder function

--- export/models.py
+++ export/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Guest(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    base64_image = models.TextField() 
+    base64_image = models.TextField()
+
+    def __str__(self):
+        return str(self.first_name + " " + self.last_name)
