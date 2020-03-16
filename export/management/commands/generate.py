29e003842b9be8d9fd50b4377f4f1603859a6292
kormosi <kormi.patres@gmail.com>
2020-03-16 21:34:36 +0100

    implement bulk_create() method for generating test guests

--- export/management/commands/generate.py
+++ export/management/commands/generate.py
@@ -1,25 +1,30 @@
 from django.core.management.base import BaseCommand, CommandError
 from export.models import Guest
-import random, string
 from export.management.commands.image import image
+import random, string
 
 class Command(BaseCommand):
 
     help = "Generate N dummy guests and add them to database."
 
     def add_arguments(self, parser):
-        parser.add_argument('amount', type=int, help="Indicates the number of Guests to be created.")
+        parser.add_argument(
+                'amount', type=int,
+                 help="Indicates the number of Guests to be created."
+                 )
 
 
     def handle(self, *args, **kwargs):
-
         amount = kwargs['amount']
 
+        # Generate a random word to be used as first and last names.
         def randomword(length):
            letters = string.ascii_lowercase
            return ''.join(random.choice(letters) for i in range(length))
 
-        for i in range(amount):
-          self.stdout.write(str(i))
-          guest = Guest(first_name=f"{randomword(7)}", last_name=f"{randomword(10)}", base64_image=image)
-          guest.save()
+        # Create n Guest instances in bulk.
+        Guest.objects.bulk_create([
+            Guest(first_name=f"{randomword(7)}",
+                  last_name=f"{randomword(10)}",
+                  base64_image=image) for i in range(amount)
+                  ])
